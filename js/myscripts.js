efe3d311ff0994804b83e856ac10d3197eac5c2c
moustafaomar <moustafaomar200@gmail.com>
2016-07-18 03:34:49 -0700

    Removed some extra not needed JS

--- js/myscripts.js
+++ js/myscripts.js
@@ -8,7 +8,6 @@ $(function () {
     
     var myHeader = $('.header'),
         mySlider = $('.bxslider'),
-        $messages = $('.error-message-wrapper');
     
     myHeader.height($(window).height());
     
@@ -55,30 +54,6 @@ $(function () {
         }
     });
     
-    //Triger Mixitup
-    $('.gallery').mixItUp();
-    //Adjust shuffle links
-    
-    $('.shuffle li').click(function () {
-    
-        $(this).addClass('selected').siblings().removeClass('selected');
-        
-    
-    });
-    
-   
-    //Color on click
-    
-    $('.form .form-input').click(function () {
-        $(this).addClass('input-selected').siblings().removeClass('input-selected');
-        
-    });
-    $.validate({
-        errorMessagePosition : $messages,
-        scrollToTopOnError : false // Set this property to true if you have a long form
-    });
-    
-    
     //scroll to top fade-out on scroll
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
@@ -88,44 +63,5 @@ $(function () {
         }
     });
     
-    //Custom Auto Slider
-    (function AutoSlider() {
-        $('.work .opinions .slider .active').each(function () {
     
-            if (!$(this).is(':last-child')) {
-                $(this).delay(3000).fadeOut(1000 , function (){
-                
-                $(this).removeClass('active').next().addClass('active').fadeIn();
-                AutoSlider();
-                
-                });
-    
-            }
-            else{
-                $(this).delay(5000).fadeOut(1000 , function (){
-                    $(this).removeClass('active');
-                    $('.work .opinions .slider > div').eq(0).addClass('active').fadeIn();
-                    AutoSlider();
-                    
-                });
-
-            }
-        
-        
-        });
-        
-        
-        
-    }());
-//Spread Bars navbar
-    
-    $('#hidden_nav').click(function(){
-    if( $(".links").css('display') == 'block') {
-        $('.links').css('display' , 'none');
-    }
-        else {
-                $('.links').css('display' , 'block');
-            }
-    
-    });
 });
